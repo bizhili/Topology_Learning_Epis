d98ad530cb047162ad57039a509de56c1e408a2f
Asil thinkbook 14+ <moonbizhi@gmail.com>
2023-12-10 00:13:50 +0800

    update from laptop

--- myCommands.sh
+++ myCommands.sh
@@ -3,3 +3,6 @@ python run.py --epoches 140000 --modelLoad AA --weightModel identical --randomGr
 echo "python run.py --epoches 140000 --modelLoad AA --weightModel identical --randomGraph BA --seed 14 --strains 3 --CMDprogress_79_79"
 python run.py --epoches 140000 --modelLoad AA --weightModel identical --randomGraph BA --seed 14 --strains 3 --CMDprogress_79_79 &
 wait
+
+python run.py --epoches 140000 --modelLoad AA --weightModel identical --randomGraph BA --seed 14 --strains 3 --CMDprogress_79_79 &
+
