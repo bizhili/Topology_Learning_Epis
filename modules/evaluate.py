24b287a9d3b199fa26e7085a3e25403ee206efb0
Asil thinkbook 14+ <moonbizhi@gmail.com>
2024-01-03 11:36:15 +0800

    update feom laptop

--- modules/evaluate.py
+++ modules/evaluate.py
@@ -1,4 +1,7 @@
 import torch
+import numpy as np
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
 
 
 def square_error(Z, preZ):
@@ -44,4 +47,26 @@ def normalized_hamming_distance(Z, preZ):
 
     return normalized_distance
 
+def draw_auc_roc(A, preA):
+    # Flatten matrices into 1D arrays
+    y_true = A.numpy().flatten()
+    y_pred = preA.numpy().flatten()
 
+    # Calculate false positive rate, true positive rate, and thresholds
+    fpr, tpr, thresholds = roc_curve(y_true, y_pred, pos_label=1)
+    print(fpr)
+
+    # Calculate AUC
+    roc_auc = auc(fpr, tpr)
+
+    # Plot ROC curve
+    plt.figure()
+    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
