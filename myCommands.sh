f8836bb864aa9dd683eb27093a9baf8ace42b307
Asil thinkbook 14+ <moonbizhi@gmail.com>
2024-03-28 16:42:33 +0200

    update form laptop

--- myCommands.sh
+++ myCommands.sh
@@ -1,18 +1,72 @@
-echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_0_7"
-python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_0_7 &
-echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph global_air        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_1_7"
-python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph global_air        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_1_7 &
-echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph global_air        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_2_7"
-python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph global_air        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_2_7 &
-echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_3_7"
-python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_3_7 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air200        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_0_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air200        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_0_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air100        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_1_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air100        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_1_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air100        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_2_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air100        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_2_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air400        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_3_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air400        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_3_31 &
 wait
-echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_4_7"
-python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_4_7 &
-echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph global_air        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_5_7"
-python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph global_air        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_5_7 &
-echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_6_7"
-python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_6_7 &
-echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph global_air        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_7_7"
-python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph global_air        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_7_7 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_car        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_4_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_car        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_4_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_train        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_5_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_train        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_5_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_plane        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_6_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_plane        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_6_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_train        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_7_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_train        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_7_31 &
+wait
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air300        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_8_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air300        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_8_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_bus        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_9_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_bus        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_9_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_bus        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_10_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_bus        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_10_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_car        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_11_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_car        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_11_31 &
+wait
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_bus        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_12_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_bus        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_12_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_plane        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_13_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_plane        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_13_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air300        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_14_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air300        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_14_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_train        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_15_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_train        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_15_31 &
+wait
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air400        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_16_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air400        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_16_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air100        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_17_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air100        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_17_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air200        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_18_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air200        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_18_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_car        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_19_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_car        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_19_31 &
+wait
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air200        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_20_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air200        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_20_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air300        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_21_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air300        --seed 11 --strains 1  --dense 0 --n 100 --intense -1 --CMDprogress_21_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air400        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_22_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air400        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_22_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_plane        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_23_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_plane        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_23_31 &
+wait
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air400        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_24_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air400        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_24_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_train        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_25_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_train        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_25_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air200        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_26_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air200        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_26_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_plane        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_27_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_plane        --seed 11 --strains 4  --dense 0 --n 100 --intense -1 --CMDprogress_27_31 &
+wait
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air100        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_28_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air100        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_28_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air300        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_29_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph us_air300        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_29_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_car        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_30_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_car        --seed 11 --strains 2  --dense 0 --n 100 --intense -1 --CMDprogress_30_31 &
+echo "python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_bus        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_31_31"
+python run.py --epoches 150000 --modelLoad AA --weightModel identical --randomGraph spain_bus        --seed 11 --strains 3  --dense 0 --n 100 --intense -1 --CMDprogress_31_31 &
 wait
