579ed23db4b1dbd74e455d784e1d1c2b3ffaae72
Asil <moonbizhi@gmail.com>
2025-03-23 17:44:29 +0200

    update from cluster

--- gitCommon.sh
+++ gitCommon.sh
@@ -2,5 +2,6 @@ git add .
 git commit -m "update from cluster"
 git push -u origin master
 
-# git reset --soft HEAD~1
+# git reset --soft HEAD~1 #del latt commit
 # git log
+# git push -u origin master --force-with-lease #del last commit if pushed
